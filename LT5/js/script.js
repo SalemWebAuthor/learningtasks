84ad2e04771d56e7e9b5fa6aa628e12e38f4abc9
SalemWebAuthor <100058945+SalemWebAuthor@users.noreply.github.com>
2022-04-25 20:10:00 +0800

    Add files via upload

---
+++ LT5/js/script.js
@@ -0,0 +1,57 @@
+function buttonclick1() {
+  document.getElementById("img1").style.visibility = "visible";
+    alert ("You got an apple!");
+}
+
+function buttonclick2() {
+  document.getElementById("img2").style.visibility = "visible";
+    alert ("You got a book!");
+}
+
+function buttonclick3() {
+  document.getElementById("img3").style.visibility = "visible";
+    alert ("You got a beach villa!");
+}
+
+function buttonclick4() {
+  document.getElementById("img4").style.visibility = "visible";
+    alert ("You got a cat!");
+}
+
+function buttonclick5() {
+  document.getElementById("img5").style.visibility = "visible";
+    alert ("You got a car!");
+}
+
+function buttonclick6() {
+  document.getElementById("img6").style.visibility = "visible";
+    alert ("You got money!");
+}
+
+function buttonclick7() {
+  document.getElementById("img7").style.visibility = "visible";
+    alert ("You got a smiling dog!");
+}
+
+function buttonclick8() {
+  document.getElementById("img8").style.visibility = "visible";
+    alert ("You got a monkey!");
+}
+
+function buttonclick9() {
+  document.getElementById("img9").style.visibility = "visible";
+    alert ("You got a gun!");
+}
+
+function buttonclick10() {
+  document.getElementById("img10").style.visibility = "visible";
+    alert ("You got a goldfish!");
+}
+
+function changeBgColor(obj, colorName) {
+  obj.style.backgroundColor = colorName;
+}
+
+
+
+
